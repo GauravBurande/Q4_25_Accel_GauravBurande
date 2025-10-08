c118b4eaec8bb54dae0d76cef5242cd7744a5e1b
Gaurav <burandegaurav8899@gmail.com>
2025-10-09 00:59:14 +0530

    feat: whitelist as a pda per user (not tested)

---
+++ whitelist-transfer-hook/programs/whitelist-transfer-hook/src/error/whitelist_error.rs
@@ -0,0 +1,11 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum WhitelistError {
+    #[msg("This account is not whitelisted")]
+    NotWhitelisted,
+    #[msg("This account is already whitelisted")]
+    AlreadyWhitelisted,
+    #[msg("This account is not the admin")]
+    NotAdmin
+}
---
+++ whitelist-transfer-hook/programs/whitelist-transfer-hook/src/instructions/whitelist_operations.rs
@@ -0,0 +1,48 @@
+use anchor_lang::{
+    prelude::*, system_program, 
+};
+use crate::{error::WhitelistError, state::{Whitelist}};
+
+#[derive(Accounts)]
+#[instruction(user:Pubkey)]
+pub struct WhitelistOperations<'info> {
+    #[account(
+        mut,
+        //address = <the admin address> (to be more strict)
+    )]
+    pub admin: Signer<'info>,
+    #[account(
+        init_if_needed,
+        payer=admin,
+        space= 8 + Whitelist::INIT_SPACE,
+        seeds = [b"whitelist", user.key().as_ref()],
+        bump,
+    )]
+    pub whitelist: Account<'info, Whitelist>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> WhitelistOperations<'info> {
+    pub fn add_to_whitelist(&mut self, user: Pubkey, bumps:&WhitelistOperationsBumps) -> Result<()> {
+
+        if self.whitelist.address == user {
+            return err!(WhitelistError::AlreadyWhitelisted);
+        };
+
+        self.whitelist.set_inner(Whitelist { address: user, bump: bumps.whitelist});
+        Ok(())
+    }
+
+    pub fn remove_from_whitelist(&mut self, user: Pubkey) -> Result<()> {
+        if self.whitelist.address != user {
+            return  err!(WhitelistError::NotWhitelisted);
+        };
+
+        let rent_lamports = **self.whitelist.to_account_info().lamports.borrow();
+        **self.admin.to_account_info().lamports.borrow_mut() += rent_lamports;
+        **self.whitelist.to_account_info().lamports.borrow_mut() -= rent_lamports;
+        self.whitelist.to_account_info().assign(&self.system_program.key());
+
+        Ok(())
+    }
+}
